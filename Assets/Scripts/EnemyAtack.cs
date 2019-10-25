9908418906da3dfc3a265f3d15c8abc6fc55d3c1
Kto-to_123 <nikolai.sv13@gmail.com>
2019-10-25 15:39:30 +0300

    Enemy attack system
    Добавление врагам возможности атаковать игрока

---
+++ Assets/Scripts/EnemyAtack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAtack : MonoBehaviour
+{
+    public GameObject Hammer;
+    Animator hammerAnimator;
+
+    private void Start()
+    {
+        hammerAnimator = Hammer.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            hammerAnimator.SetTrigger("Attack");
+    }
+}
---
+++ Assets/Scripts/Hammer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    public Transform Enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerManager player = other.GetComponent<PlayerManager>();
+            if (player != null)
+            {
+                player.GetComponent<Rigidbody>().AddForce((player.transform.position - Enemy.position).normalized * 300, ForceMode.Impulse);
+            }
+        }
+    }
+}
