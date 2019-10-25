b4995a6e02ad246412011d8e3d20912b9c54ad3d
Kto-to_123 <nikolai.sv13@gmail.com>
2019-10-25 22:56:48 +0300

    Trap system
    Добавление возможности установки ловушек

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,8 +7,9 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public WeaponData myWeaponData;
-
     public static Weapon instance;
+
+    [SerializeField] Transform cameraT;
 
     // Основное оружие
     bool mainWeaponInst = false;
@@ -20,8 +21,14 @@ public class Weapon : MonoBehaviour
     public bool throwingWeaponActive = false;
     public int throwingAmmunition = 0;
     ElementThrowingWeapons throwingElement;
-    // Метательное оружие не имеет своего класа как основное т.к поведение разных 
-    // типов не сильно отличается.
+    // Метательное оружие не имеет своего класа как основное т.к поведение разных типов не отличается.
+
+    //Ловушки
+    //Разработка системы установки ловушек и копканов
+    public GameObject trapGhost;
+    GameObject trapGhostInstanse;
+    public GameObject trap;
+
 
     private void Awake()
     {
@@ -104,5 +111,47 @@ public class Weapon : MonoBehaviour
         throwingWeaponActive = true;
         throwingAmmunition = _ammunition;
         UIManager.instance.SetWeapon(throwingElement.ID, throwingAmmunition);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            TrapSetting();
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            TrapIstanse();
+        }
+    }
+
+    public void TrapSetting()
+    {
+        Ray ray = new Ray(cameraT.position, cameraT.forward * 10f);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10f, 13, QueryTriggerInteraction.Ignore))
+        {
+            if (trapGhostInstanse == null)
+            {
+                trapGhostInstanse = Instantiate<GameObject>(trapGhost);
+                trapGhostInstanse.transform.position = hit.point + hit.normal * 0.01f;
+                trapGhostInstanse.transform.rotation = Quaternion.LookRotation(-hit.normal);
+                //trapGhostInstanse.transform.SetParent(hit.transform);
+            }
+            else
+            {
+                trapGhostInstanse.transform.position = hit.point + hit.normal * 0.01f;
+                trapGhostInstanse.transform.rotation = Quaternion.LookRotation(-hit.normal);
+                //trapGhostInstanse.transform.SetParent(hit.transform);
+            }
+        }
+    }
+
+    public void TrapIstanse()
+    {
+        var NewTrap = Instantiate(trap);
+        NewTrap.transform.position = trapGhostInstanse.transform.position;
+        NewTrap.transform.rotation = trapGhostInstanse.transform.rotation;
+        GameObject.Destroy(trapGhostInstanse);
     }
 }
